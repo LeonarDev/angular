174031845c5aea2d634cfc234ac53e57af206acc
LeonarDev <leonardo.majevski@gmail.com>
2021-08-16 20:44:17 -0300

    Event binding

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 @NgModule({
   declarations: [
     AppComponent,
     DataBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export class AppBootstrapModule {}  
--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,12 +3,41 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  // styleUrls: ['./data-binding.component.css']
+  styles: [
+    `
+      .highlight {
+          background-color: yellow;
+          font-weight: bold;
+      }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
-  urlImagem1: string = "http://lorempixel.com.br/500/400/?1";
-  urlImagem2: string = "http://lorempixel.com.br/500/400/?2";
-  urlImagem3: string = "http://lorempixel.com.br/500/400/?3";
+  urlImagem1: string = "http://lorempixel.com.br/300/200/?1";
+  urlImagem2: string = "http://lorempixel.com.br/300/200/?2";
+  urlImagem3: string = "http://lorempixel.com.br/300/200/?3";
+
+  valorAtual: string = '';
+  valorSalvo: string = '';
+
+  isMouseOver: boolean = false;
+
+  botaoClicado() {
+    alert("Botão clicado!")
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value
+  }
+
+  salvarValor(valor: string) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
 
   constructor() { }
 
